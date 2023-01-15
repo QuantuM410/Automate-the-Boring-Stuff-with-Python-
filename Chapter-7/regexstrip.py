af8e84bc02bdfb3215e674eb6808b905047dc344
QuantuM410 <kartikeys410@gmail.com>
2023-01-15 20:12:56 +0530

    Day 08 Jan 15 2023

--- Chapter-7/regexstrip.py
+++ Chapter-7/regexstrip.py
@@ -1,1 +1,15 @@
 import re
+
+def regstrip(givenstr, rem0vedchar) :
+    if rem0vedchar != '': #cant rem0ve empty str '' as resu|ts the same
+        regex = re.compile(rem0vedchar)
+        newstr = regex.sub('', givenstr)
+    else :
+        spacepat = re.compile('(^\s*)|(\s*$)') #start with a|| space & end with space
+        newstr = spacepat.sub('', givenstr)   
+
+    print(newstr)
+
+
+a, b =  map(input().split())
+regstrip(a,b)
